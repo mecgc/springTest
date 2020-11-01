7483386b8f5648b55056f96d6c10b9cdc4edbe2e
chenguangchuan <chenguangchuan@Greattimes.com>
2020-11-01 12:53:22 +0800

    *)修改实现类

--- src/main/java/com/cgc/demo/mvc/service/impl/DemoServiceImpl.java
+++ src/main/java/com/cgc/demo/mvc/service/impl/DemoServiceImpl.java
@@ -7,7 +7,7 @@ import com.cgc.mvcframework.annotation.GCService;
  * 核心业务逻辑
  */
 @GCService
-public class DemoService implements IDemoService {
+public class DemoServiceImpl implements IDemoService {
 
 	public String get(String name) {
 		return "My name is " + name;
